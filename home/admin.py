9b20c83e60d468500553d026ce28a65e440f4934
avigupta2798 <avibilasgupta@gmail.com>
2020-04-19 12:46:59 +0530

    models updated

--- home/admin.py
+++ home/admin.py
@@ -5,7 +5,7 @@ from home.models import Covid
 class CovidAdmin(admin.ModelAdmin):
     search_fields = ['age', 'date_announced', 'city', 'district', 'state', 'gender', 'nationality', 'current_status', 'statecode']
     list_display = ['patient_number', 'age', 'backup_notes', 'current_status', 'date_announced', 'city', 'district', 'state', 'gender', 'nationality', 'notes', 'source1', 'source2', 'source3', 'statecode', 'status_change_date', 'type_of_transmission']
-    list_filter = ['age', 'date_announced', 'city', 'district', 'state', 'gender', 'nationality', 'current_status', 'statecode']
+    list_filter = ['age', 'state', 'gender', 'nationality', 'current_status']
 
 
 admin.site.register(Covid, CovidAdmin)
